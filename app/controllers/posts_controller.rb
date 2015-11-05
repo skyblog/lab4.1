03ce9e1bc1de748af94486263538476d4e24a894
saasbook <saasbook@saasbook.(none)>
2015-11-05 08:30:12 -0400

    My blog index view createed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,43 @@
 class PostsController < ApplicationController
+	
 	def index
 		@posts=Post.all
 	end
 
 	def show
+		@post= Post.find(params[:id])
 	end
 
 	def new
+		@post=Post.new
 	end
 
 	def create
+		@post= Post.new(params[:post])#<- older version
+		#@post = Post.new(params.require(:post).permit(:title,:content))
+		if @post.save
+			redirect_to posts_path,:notice => "Successfully created!"
+		else
+			render"new"
+		end
+		
 	end
 
 	def edit
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+		@post.update_attributes!(params[:post])
+		flash[:notice] = "#{@post.title} was successfully updated"
+		redirect_to post_path(@post)
 	end
 
-	def destory
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path, :notice => "post '#{@post.title}' deleted"
 	end
 	
 end
